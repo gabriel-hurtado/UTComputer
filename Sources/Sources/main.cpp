1812d5e64ae99380c6787a312d77302b3e13bfc1
Gabriel Hurtado <gabriel.j.hurtado@gmail.com>
2016-05-24 12:42:41 +0200

    Working fine !

--- Sources/Sources/main.cpp
+++ Sources/Sources/main.cpp
@@ -6,9 +6,10 @@ int main() {
 
 	Rationnel r = Rationnel(13, 5);
 	Rationnel r2 = Rationnel(12, 5);
+    Entier e= Entier(5);
     Litterale* ptr = &r;
-    OperateurAddition ad = OperateurAddition(&r,&r2);
-    ptr = estdeType<Rationnel>(ad.traitement());
+    OperateurAddition ad = OperateurAddition(&r,&e);
+    ptr = ad.traitement();
     ptr->afficher();
     //system("pause");
     if(estdeType<Rationnel>(ptr))
--- Sources/Sources/operateur.cpp
+++ Sources/Sources/operateur.cpp
@@ -5,16 +5,29 @@ Litterale* OperateurAddition::traitementOperateur(){
     Rationnel* r1=estdeType<Rationnel>(l1);
     Rationnel* r2=estdeType<Rationnel>(l2);
 
-    if(r1 && r2){
+    if(r1 && r2 ){
         int newDen=r1->getDenominator().getValeur()*r2->getDenominator().getValeur();
         int newNum = r1->getNumerateur().getValeur()*r2->getDenominator().getValeur()+r2->getNumerateur().getValeur()*r1->getDenominator().getValeur();
-        return (new Rationnel(newDen, newNum));
+        return (new Rationnel(newNum, newDen));
     }
 
     Entier* e1=estdeType<Entier>(l1);
     Entier* e2=estdeType<Entier>(l2);
     if(e1 && e2){
         return ( new Entier(e1->getValeur()+e2->getValeur()));
+    }
+
+    if(r1 && e2){
+       Rationnel temp= Rationnel(e2->getValeur());
+       OperateurAddition ad = OperateurAddition(r1,&temp);
+       return ad.traitement();
+
+    }
+    if(r2 && e1){
+       Rationnel temp= Rationnel(e1->getValeur());
+       OperateurAddition ad = OperateurAddition(r2,&temp);
+       return ad.traitement();
+
     }
 }
 
